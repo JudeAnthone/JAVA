d9ffb404027b4a0039f9d22fa5ad3d32338aa58e
JudeAnthone <151014535+JudeAnthone@users.noreply.github.com>
2024-09-27 00:58:16 +0800

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,20 @@
+//polymorphism 
+package java_fundamentals;
+
+public class Animal {
+	
+	public void makeSound() {
+		System.out.println("This animal make sound");
+	}
+	
+	public void bark() {
+		
+	}
+	
+	public void meow () {
+		
+	}
+	
+
+
+}
